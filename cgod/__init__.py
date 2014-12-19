bc9754d53d422a3f069f36a630493338f686d3f4
James Mills <prologic@shortcircuit.net.au>
2014-12-19 21:01:42 +1000

    Added a proper packaging structure

---
+++ cgod/__init__.py
@@ -0,0 +1,18 @@
+# Package:  cgod
+# Date:     19th December 2014
+# Author:   James Mills, prologic at shortcircuit dot net dot au
+
+
+"""cgod - circuits Gopher Daemon
+
+cgod is a Gopher Daemon using the circuits framework.
+
+:copyright: CopyRight (C) 2014 by James Mills
+"""
+
+
+__author__ = "James Mills, prologic at shortcircuit dot net dot au"
+__date__ = "19th December 2014"
+
+
+from .version import version as __version__  # noqa
